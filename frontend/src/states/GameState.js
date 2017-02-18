31ffe4e59df20f5b24aef52988c6790bd74c979b
Gabriel Elkind <gwelkind@gmail.com>
2017-02-18 15:08:06 -0600

    Set up from new boilerplate

---
+++ frontend/src/states/GameState.js
@@ -0,0 +1,32 @@
+import RainbowText from 'objects/RainbowText';
+let io = require('socket.io-client');
+
+class GameState extends Phaser.State {
+
+	update() {
+		console.log('foo');
+	}
+
+	create() {
+		let center = { x: this.game.world.centerX, y: this.game.world.centerY }
+		// let text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
+		// text.anchor.set(0.5);
+
+		this._speed = 150;
+
+		this.connection = io('localhost:3001');
+
+		this.connection.on('send-game-state', (state) => {
+			console.log(state);
+		});
+
+		this.connection.on('player-created', (player) => {
+			console.log(state);
+		});
+
+    this.game.time.events.loop(this._speed, this.update, this).timer.start();
+	}
+
+}
+
+export default GameState;
